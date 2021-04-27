24884e6d830be585941177da03ce760cb7c702e9
kanou7 <kanounba7@icloud.com>
2021-04-27 14:40:45 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :deli_price_id
+    validates :area_id
+    validates :deli_day_id
+  end
 end
