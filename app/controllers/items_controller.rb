406a7d8c40fd5a9e2306595339a26f561f67db92
kanou7 <kanounba7@icloud.com>
2021-04-30 12:44:59 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,9 +46,6 @@ class ItemsController < ApplicationController
   def move_to_index
     @item = Item.find(params[:id])
     @user = User.find(@item.user_id)
-    if current_user.id != @user.id
-      redirect_to action: :index
-    end
+    redirect_to action: :index if current_user.id != @user.id
   end
-
 end
