fc1b2b63b5d3cb0ef453d0ca4d97505a9cc4301e
kanou7 <kanounba7@icloud.com>
2021-05-04 18:59:47 +0900

    formオブジェクトテスト

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -5,7 +5,7 @@ class PurchaseAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefectures, numericality: { other_than: 1 }
     validates :municipality
     validates :address
---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :purchase_address do
+    postal_code { '123-4567' }
+    prefectures { 2 }
+    municipality { '横浜市緑区' }
+    address { '1-1' }
+    building { '東京ハイツ' }
+    phone_num { '11111111111'}
+  end
+end
